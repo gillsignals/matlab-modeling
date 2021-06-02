ee976af87f2d477b9e4643b21c08155dd76f3026
Amy Gill <gill.signals@gmail.com>
2021-06-02 09:58:01 -0400

    Adding parameter labels in comments.

--- intro_parameters.m
+++ intro_parameters.m
@@ -1,18 +1,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Parameters
 
-p.V             = 0.1;  % mL
-p.qA            = 2e-7; % nM/s
-p.qB            = 1e-7; % nM/s
-p.kclA          = 3e-6; % s-1
-p.kclB          = 1e-6; % s-1
-p.kon_A_RA      = 3e-4; % nM-1 s-1
-p.koff_A_RA     = 1e-2; % s-1
-p.kon_B_RB      = 8e-4; % nM-1 s-1
-p.koff_B_RB     = 5e-3; % s-1
-p.kon_A_RA_CoR  = 2e-4; % nM-1 s-1
-p.koff_A_RA_CoR = 2e-2; % s-1
-p.kon_B_RB_CoR  = 1e-4; % nM-1 s-1
-p.koff_B_RB_CoR = 3e-2; % s-1
+p.V             = 0.1;  % mL ... volume
+p.qA            = 2e-7; % nM/s ... production rate of A
+p.qB            = 1e-7; % nM/s ... production rate of B
+p.kclA          = 3e-6; % s-1 ... clearance rate of A
+p.kclB          = 1e-6; % s-1 ... clearance rate of B
+p.kon_A_RA      = 3e-4; % nM-1 s-1 ... binding rate of A to RA
+p.koff_A_RA     = 1e-2; % s-1 ... unbinding rate of A from RA
+p.kon_B_RB      = 8e-4; % nM-1 s-1 ... binding rate of B to RB
+p.koff_B_RB     = 5e-3; % s-1 ... unbinding rate of B from RB
+p.kon_A_RA_CoR  = 2e-4; % nM-1 s-1 ... binding rate of A_RA to CoR
+p.koff_A_RA_CoR = 2e-2; % s-1 ... unbinding rate of A_RA from CoR
+p.kon_B_RB_CoR  = 1e-4; % nM-1 s-1 ... binding rate of B_RB to CoR
+p.koff_B_RB_CoR = 3e-2; % s-1 ... unbinding rate of B_RB from CoR
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
