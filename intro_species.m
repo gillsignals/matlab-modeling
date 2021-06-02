82a20310e11f22b93816345c0097babf3eda91c6
Amy Gill <gill.signals@gmail.com>
2021-06-02 09:42:24 -0400

    Adding species labels in comments.

--- intro_species.m
+++ intro_species.m
@@ -1,16 +1,16 @@
 %%%%%%%%%%%%%%%%
 %% Species
 
-m.A         = 1;
-m.B         = 2;
-m.RA        = 3;
-m.RB        = 4;
-m.CoR       = 5;
-m.A_RA      = 6;
-m.B_RB      = 7;
-m.A_RA_CoR  = 8;
-m.B_RB_CoR  = 9;
-m.Acl       = 10;
-m.Bcl       = 11;
+m.A         = 1;    % ligand A
+m.B         = 2;    % ligand B
+m.RA        = 3;    % receptor for ligand A
+m.RB        = 4;    % receptor for ligand B
+m.CoR       = 5;    % co-receptor (shared)
+m.A_RA      = 6;    % ligand A bound to receptor RA
+m.B_RB      = 7;    % ligand B bound to receptor RB
+m.A_RA_CoR  = 8;    % ligand A bound to receptor RA and co-receptor
+m.B_RB_CoR  = 9;    % ligand B bound to receptor RB and co-receptor
+m.Acl       = 10;   % ? A cleared? for mass balance?
+m.Bcl       = 11;   % ? B cleared? for mass balance?
 
 %%%%%%%%%%%%%%%%%
